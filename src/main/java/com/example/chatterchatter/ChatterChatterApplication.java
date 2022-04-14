ec5b483f352d269f03f241a150a9671a8b285244
svenzioerjen-fhnw <sven.zioerjen@students.fhnw.ch>
2022-04-14 14:41:39 +0200

    initial commit

---
+++ src/main/java/com/example/chatterchatter/ChatterChatterApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatterchatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterChatterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatterChatterApplication.class, args);
+    }
+
+}
