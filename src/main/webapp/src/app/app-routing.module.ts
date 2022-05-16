7209f4ced4bbe971b4bf8a3c37d348f1bcfe39ad
Jan Zimmermann <jaenu.zimmermann@gmail.com>
2022-05-16 07:45:33 +0200

    Remove hello world components

--- src/main/webapp/src/app/app-routing.module.ts
+++ src/main/webapp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {HelloWorldComponent} from "./main/hello-world/hello-world.component";
 import {LoginComponent} from "./main/login/login.component";
 import {RegisterComponent} from "./main/register/register.component";
 import {UserListComponent} from "./main/user-management/user-list/user-list.component";
@@ -15,7 +14,6 @@ import {AdminGuard} from "./guard/admin.guard";
 const routes: Routes = [
   {path: 'chat', component: SubjectComponent, canActivate: [AuthenticationGuard]},
   {path: 'chat/:id', component: ChatComponent, canActivate: [AuthenticationGuard]},
-  {path: 'hello', component: HelloWorldComponent},
   {path: 'login', component: LoginComponent},
   {path: 'logout', component: LogoutComponent, canActivate: [AuthenticationGuard]},
   {path: 'register', component: RegisterComponent, canActivate: [AuthenticationGuard]},
