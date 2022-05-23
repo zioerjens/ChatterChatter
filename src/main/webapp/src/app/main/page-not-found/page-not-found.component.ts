1123c9a65b45b50da22b09b93fba44a7757acb93
svenzioerjen-fhnw <sven.zioerjen@students.fhnw.ch>
2022-05-23 08:11:26 +0200

    added login-button on the 404 page if user is not logged in

--- src/main/webapp/src/app/main/page-not-found/page-not-found.component.ts
+++ src/main/webapp/src/app/main/page-not-found/page-not-found.component.ts
@@ -1,16 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {AuthenticationService} from "../../service/authentication.service";
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
-export class PageNotFoundComponent implements OnInit {
+export class PageNotFoundComponent {
 
-  constructor() {
+  private isLoggedIn = false;
+
+  constructor(
+    private authService: AuthenticationService
+  ) {
+    this.authService.isLoggedIn().subscribe(v => {
+      this.isLoggedIn = v;
+    });
   }
 
-  ngOnInit(): void {
+  isNotLoggedIn(): boolean {
+    return !this.isLoggedIn;
   }
 
 }
